e534608c3828624a525712c9ffc8cf84ce939ffd
Bregje Mastenbroek <110273@st-maartenscollege.nl>
2020-01-17 12:48:02 +0100

    show all pagina toegevoegd

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -57,6 +57,7 @@ namespace SchoolTemplate.Controllers
             return View();
         }
 
+        [Route("show-all")]
         public IActionResult ShowAll()
         {
             return View();
